b39687cdbcc59b8407909be3be861faeeeed7aac
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-03-30 18:53:20 +0200

    Add project files.

---
+++ Vakantieverhuur.WPF/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Vakantieverhuur.LIB.Entities;
+using Vakantieverhuur.LIB.Services;
+
+namespace Vakantieverhuur.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        List<Verblijf> verblijven;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Verblijven.Initialiseer();
+            verblijven = Verblijven.AlleVerblijven;
+            lstVerblijven.Items.Clear();
+            foreach (Verblijf verblijf in verblijven)
+            {
+                lstVerblijven.Items.Add(verblijf);
+            }
+        }
+        private void cmbSoorten_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!this.IsLoaded) return;
+
+            lstVerblijven.Items.Clear();
+            if (cmbSoorten.SelectedIndex == 0)
+            {
+                foreach (Verblijf verblijf in verblijven)
+                {
+                    lstVerblijven.Items.Add(verblijf);
+                }
+            }
+            else if (cmbSoorten.SelectedIndex == 1)
+            {
+                foreach(Verblijf verblijf in verblijven)
+                {
+                    if(verblijf is Vakantiehuis)
+                    {
+                        lstVerblijven.Items.Add(verblijf);
+                    }
+                }
+            }
+            else
+            {
+                foreach (Verblijf verblijf in verblijven)
+                {
+                    if (verblijf is Caravan)
+                    {
+                        lstVerblijven.Items.Add(verblijf);
+                    }
+                }
+            }
+
+        }
+
+        private void BtnView_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstVerblijven.SelectedIndex == -1) return;
+            winVerblijven venster = new winVerblijven();
+            venster.situatie = "view";
+            venster.verblijf = (Verblijf)lstVerblijven.SelectedItem;
+            venster.ShowDialog();
+        }
+
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+            winVerblijven venster = new winVerblijven();
+            venster.situatie = "new";
+            venster.verblijf = null;
+            venster.ShowDialog();
+
+            cmbSoorten_SelectionChanged(null, null);
+            lstVerblijven.SelectedItem = venster.verblijf;
+
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstVerblijven.SelectedIndex == -1) return;
+
+            winVerblijven venster = new winVerblijven();
+            venster.situatie = "edit";
+            venster.verblijf = (Verblijf)lstVerblijven.SelectedItem;
+            venster.ShowDialog();
+
+            cmbSoorten_SelectionChanged(null, null);
+            lstVerblijven.SelectedItem = venster.verblijf;
+
+        }
+
+
+        private void BtnVerhuurNew_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstVerblijven.SelectedIndex == -1) return;
+
+            if (!((Verblijf)lstVerblijven.SelectedItem).Verhuurbaar)
+            {
+                MessageBox.Show("Dit verblijf kan momenteel niet verhuurd worden", "Niet toegelaten", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            winVerhuur venster = new winVerhuur();
+            venster.situatie = "new";
+            venster.verblijf = (Verblijf)lstVerblijven.SelectedItem;
+            venster.ShowDialog();
+        }
+    }
+}
---
+++ Vakantieverhuur.WPF/winVerhuur.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Vakantieverhuur.LIB.Entities;
+using Vakantieverhuur.LIB.Services;
+
+namespace Vakantieverhuur.WPF
+{
+    /// <summary>
+    /// Interaction logic for winVerhuur.xaml
+    /// </summary>
+    public partial class winVerhuur : Window
+    {
+        public winVerhuur()
+        {
+            InitializeComponent();
+        }
+
+        public string situatie = "";
+        public Verblijf verblijf;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            vulWoningGegevens();
+        }
+
+        private void vulWoningGegevens()
+        {
+            if (verblijf is Vakantiehuis)
+            {
+                chkPersoonlijkSanitair.Visibility = Visibility.Hidden;
+                chkVaatwas.Visibility = Visibility.Visible;
+                chkWasmachine.Visibility = Visibility.Visible;
+                chkHoutkachel.Visibility = Visibility.Visible;
+
+                chkVaatwas.IsChecked = ((Vakantiehuis)verblijf).Vaatwas;
+                chkWasmachine.IsChecked = ((Vakantiehuis)verblijf).Wasmachine;
+                chkHoutkachel.IsChecked = ((Vakantiehuis)verblijf).Houtkachel;
+                chkPersoonlijkSanitair.IsChecked = false;
+
+            }
+            else
+            {
+                chkPersoonlijkSanitair.Visibility = Visibility.Visible;
+                chkVaatwas.Visibility = Visibility.Hidden;
+                chkWasmachine.Visibility = Visibility.Hidden;
+                chkHoutkachel.Visibility = Visibility.Hidden;
+
+                chkVaatwas.IsChecked = false;
+                chkWasmachine.IsChecked = false;
+                chkHoutkachel.IsChecked = false;
+                chkPersoonlijkSanitair.IsChecked = ((Caravan)verblijf).PersoonlijkSanitair;
+
+            }
+            txtHuisNaam.Text = verblijf.HuisNaam;
+            txtStraatEnNummer.Text = verblijf.StraatEnNummer;
+            txtPostnummer.Text = verblijf.Postnummer.ToString();
+            txtGemeente.Text = verblijf.Gemeente;
+            txtBasisPrijs.Text = verblijf.BasisPrijs.ToString();
+            txtVerminderdePrijs.Text = verblijf.VerminderdePrijs.ToString();
+            txtDagenVoorVermindering.Text = verblijf.DagenVoorVermindering.ToString();
+            txtWaarborg.Text = verblijf.Waarborg.ToString();
+            txtMaxPersonen.Text = verblijf.MaxPersonen.ToString();
+            chkMicrogolf.IsChecked = verblijf.Microgolf;
+            chkTV.IsChecked = verblijf.TV;
+            chkPersoonlijkSanitair.IsChecked = false;
+
+        }
+    }
+}
